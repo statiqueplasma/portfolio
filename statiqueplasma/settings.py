7191024e13a5b252c5a3cd2582a472cc7306ed94
statiqueplasma <h.aouadidrissi@gmail.com>
2020-10-19 20:15:44 +0100

    bdit kanmel mn hachi wlah

--- statiqueplasma/settings.py
+++ statiqueplasma/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_herku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +130,4 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_herku.settings(locals())
